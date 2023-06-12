1efde40b37989843845f5ea00ffbc79718be5eb9
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-12 22:21:18 +0700

    fix register to insert data to customer table

--- lib/ui/sign_up_screen/register_config.php
+++ lib/ui/sign_up_screen/register_config.php
@@ -19,11 +19,24 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $_SESSION['email']=$_POST['email'];
 
-$sql = "INSERT INTO users (fname, lname, uname, password, email, mobile, gender, user_type, user_status) VALUES ('$fname','-','-','$password','$email',0,'-',3,1)";
+$sql = "INSERT INTO users (fname, lname, uname, password, email, mobile, gender, user_type, user_status) VALUES ('$name','-','-','$password','$email',0,'-',3,1)";
 if ($conn->query($sql) === TRUE) {
     echo "User registered successfully!";
 } else {
     echo "Failed to register user: " . $conn->error;
+}
+$selectId = "SELECT id FROM users WHERE email='abc@bb.com';";
+$result = $conn->query($selectId);
+$row = $result->fetch_assoc();
+$idValue = $row['email'];
+$idInt = intval($idValue);
+var_dump($idValue);
+
+$sql2 = "INSERT INTO customer (user_id, region, e_credit, default_payment_method_type) VALUES ($idInt,'-',0,0);";
+if ($conn->query($sql2) === TRUE) {
+    echo "Success";
+} else {
+    echo "Failed Bye Lah" . $conn->error;
 }
 
 $conn->close();
