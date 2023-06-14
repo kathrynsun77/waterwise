5799f498cf37101b2262e7074e9e5dbc758b623c
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-14 18:49:59 +0700

    minor fixing

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       title: 'water_supply',
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.billingScreen,
+      initialRoute: AppRoutes.splashScreen,
       routes: AppRoutes.routes,
     );
   }
