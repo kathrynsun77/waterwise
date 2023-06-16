5edcd63a6c7c94cd64b6b13baade93c2f89c1a9d
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-17 02:36:49 +0700

    major data fix

--- lib/ui/login_screen/login_screen.dart
+++ lib/ui/login_screen/login_screen.dart
@@ -22,6 +22,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void loginUser(String email, String password) async {
     var url = 'http://192.168.1.16/water_wise/login_config.php';
+    // var url = 'https://waterwisee.netlify.app/login_config.php';
     var response = await http.post(Uri.parse(url), body: {
       'email': email,
       'password': password,
