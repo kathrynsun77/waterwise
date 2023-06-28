c601d5d4a2e6560081feada2b7fbb5accddd66e0
kathrynsun77 <kathrynsun77@gmail.com>
2023-06-28 18:59:29 +0800

    pull up to refresh

--- lib/ui/activity_screen/activity_screen.dart
+++ lib/ui/activity_screen/activity_screen.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:WaterWise/core/app_export.dart';
 import 'package:WaterWise/widget/custom_button.dart';
-import 'package:WaterWise/widget/custom_text_form_field.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ActivityScreen extends StatefulWidget {
