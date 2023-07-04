387c883498e72cdb5381be29f3bd834a8561b45a
kathrynsun77 <kathrynsun77@gmail.com>
2023-07-05 00:57:47 +0800

    firebase bisa

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:WaterWise/routes/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main()  {
-  WidgetsFlutterBinding.ensureInitialized();
-//   FirebaseOptions firebaseOptions = FirebaseOptions(
-//     appId: '1:669869517366:android:5d18273382c18c601d8697',
-//     apiKey: 'AIzaSyBdexCLY8cWiF96Rn429D8pJH_bWpzDKKE',
-//     projectId: 'waterwise-ac997',
-//     messagingSenderId: '669869517366',
-//     // Other options...
-//   );
-//   await Firebase.initializeApp(
-//     options: firebaseOptions
-//   );
+main()  async {
+  WidgetsFlutterBinding.ensureInitialized();;
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-//   final fcmToken = await FirebaseMessaging.instance.getToken();
-// print (fcmToken);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print ('token: ${fcmToken}');
   runApp(MyApp());
 }
 
