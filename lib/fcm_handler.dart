5e05738f92da71b6abd1c7bf87d5db40cac97d93
kathrynsun77 <kathrynsun77@gmail.com>
2023-07-27 12:58:02 +0700

    initialize in main and fcm

--- lib/fcm_handler.dart
+++ lib/fcm_handler.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseNotificationHandler {
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
@@ -19,11 +20,11 @@ class FirebaseNotificationHandler {
     // Get the FCM token and send it to the Laravel endpoint
     String? fcmToken = await _firebaseMessaging.getToken();
     if (fcmToken != null) {
-      await sendTokenToEndpoint(fcmToken);
+      await sendTokenAndNotificationToEndpoint(fcmToken,"test","test");
     }
   }
 
-  Future<void> sendTokenToEndpoint(String token) async {
+  Future<void> sendTokenAndNotificationToEndpoint(String token, String title, String body) async {
     // Replace 'YOUR_LARAVEL_ENDPOINT_URL' with the actual URL of your Laravel endpoint
     final String url = 'YOUR_LARAVEL_ENDPOINT_URL';
 
@@ -31,22 +32,50 @@ class FirebaseNotificationHandler {
     final String authorizationToken = 'YOUR_AUTHORIZATION_TOKEN';
 
     try {
-      final response = await http.post(Uri.parse(url), body: {
-        'regis': token.toString(),
-      });
+      final response = await http.post(
+        Uri.parse(url),
+        body: '{"token": "$token", "title": "$title", "body": "$body"}',
+      );
 
       if (response.statusCode == 200) {
-        print('FCM token sent to Laravel endpoint successfully.');
+        print('FCM token and notification data sent to Laravel endpoint successfully.');
       } else {
-        print('Failed to send FCM token to Laravel endpoint.');
+        print('Failed to send FCM token and notification data to Laravel endpoint.');
       }
     } catch (e) {
-      print('Error sending FCM token: $e');
+      print('Error sending FCM token and notification data: $e');
     }
   }
+
 
   static Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
     print('Handling a background message: ${message.messageId}');
     // Handle the received notification in the background as needed
+
+    // If you want to show a notification when the app is in the background or terminated, use flutter_local_notifications
+
+    // Initialize FlutterLocalNotificationsPlugin
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    // Define the notification details
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your_channel_id', // Replace 'your_channel_id' with a unique channel ID
+      'Your Channel Name', // Replace 'Your Channel Name' with a channel name for the notification
+      // 'Your Channel Description', // Replace 'Your Channel Description' with a channel description for the notification
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    // Show the notification
+    await flutterLocalNotificationsPlugin.show(
+      0, // Replace with a unique ID for each notification if needed
+      message.notification!.title, // Title of the notification received from Firebase
+      message.notification!.body, // Body of the notification received from Firebase
+      platformChannelSpecifics,
+    );
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:WaterWise/routes/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'fcm_handler.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 main()  async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +16,19 @@ main()  async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Initialize the FirebaseNotificationHandler class
+  FirebaseNotificationHandler firebaseNotificationHandler = FirebaseNotificationHandler();
+  await firebaseNotificationHandler.initialize();
+
+  // Initialize FlutterLocalNotificationsPlugin
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('app_icon'); // Replace 'app_icon' with the name of your app icon image in the 'drawable' folder
+  final InitializationSettings initializationSettings =
+  InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
   runApp(MyApp());
 }
 
